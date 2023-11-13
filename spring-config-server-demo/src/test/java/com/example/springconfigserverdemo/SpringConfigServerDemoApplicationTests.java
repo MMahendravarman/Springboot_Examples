0f6b022ebca64361cb863f2c284b7d788943abaf
MMahendravarman <mahendravarman.m@gmail.com>
2023-11-14 00:28:14 +0530

    Spring Cloud Config Server Demo

---
+++ spring-config-server-demo/src/test/java/com/example/springconfigserverdemo/SpringConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springconfigserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
